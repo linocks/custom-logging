39bbdfe560f03d385eb735197dcc1569ef520aed
linocks <knocksto1@gmail.com>
2021-05-29 21:37:00 +0000

    enable application as a servlet

--- src/main/java/com/linocks/customlogging/CustomLoggingApplication.java
+++ src/main/java/com/linocks/customlogging/CustomLoggingApplication.java
@@ -4,12 +4,19 @@ import com.linocks.customlogging.config.MyLogger;
 import io.reflectoring.descriptivelogger.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class CustomLoggingApplication {
+public class CustomLoggingApplication extends SpringBootServletInitializer {
+
+//	@Override
+//	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//		return application.sources(CustomLoggingApplication.class);
+//	}
 
 	static final MyLogger logger = LoggerFactory.getLogger(MyLogger.class, CustomLoggingApplication.class);
 	public static void main(String[] args) {
